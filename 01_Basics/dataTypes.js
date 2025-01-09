e5a2e293d48dc42f9bf441f58712696c776ae56d
Swarna940 <swarnaabhaa@gmail.com>
2025-01-09 08:21:53 +0000

    container config done

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3+3) --> we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+    // this is wrrong pratice
+
+// console.log("Riya")
+
+let name="riya"
+let age=19
+let isLoggedIn=false
+let state;
+
+// number => 2 to power 53 (2^53)
+// bigint
+// String => " " or ' '
+// boolean => true or false
+// null(special type) => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "riya"); // string
+console.log(typeof age);   // number
+console.log(typeof null);  // object
+console.log(typeof undefined); // undefined
