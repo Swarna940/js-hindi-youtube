6c0ecff761926bcb3785bd6a5ca337d30a90d147
Swarna940 <swarnaabhaa@gmail.com>
2025-01-11 15:59:42 +0000

    container confi box

---
+++ Pratice/object.js
@@ -0,0 +1,13 @@
+var ball={
+    sport : "Cricket",
+    colour : "Yellow" ,
+    radius : 3 ,
+    print:function(){
+
+    }
+}
+
+console.log(ball.sport);
+console.log(ball["radius"]);
+console.log(ball.size);
+ball.print();
