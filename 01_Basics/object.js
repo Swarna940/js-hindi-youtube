bfb157d7fa93277d50f09fb7c3e669eacf10da5b
Swarna940 <swarnaabhaa@gmail.com>
2025-01-12 19:21:45 +0000

    container config done

---
+++ 01_Basics/object.js
@@ -0,0 +1,15 @@
+var clock={
+    brand:"Kenneth Cole",
+    price:"18000",
+    color:"silver",
+    type:"automatic",
+    digital:false,
+    work:function(){
+
+    }
+}
+
+clock.brand="Titan Kenneth Cole"
+
+console.log(clock);
+console.log(clock.brand);
